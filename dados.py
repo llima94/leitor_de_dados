e24e693b2f3f89327aa99f620ff29e2364bebd70
llima94 <43402345+llima94@users.noreply.github.com>
2019-02-15 17:51:50 -0200

    Update dados.py

--- dados.py
+++ dados.py
@@ -2,23 +2,31 @@ from tkinter import *
 
 
 class Application:
-    def __init__(self, master=None):
+    def __init__(self, master=None,):
         self.widget1 = Frame(master)
-        self.widget1.grid()
+        self.widget1 = Frame(width=1024, height=768, bg='black')
+        self.widget1.pack()
         self.msg = Label(self.widget1 , text= "Dados")
-        self.msg.grid()
+        self.msg.pack()
         self.arquivo = Button(self.widget1)
         self.arquivo["text"] = "Incluar sua Planilha"
         self.arquivo["font"] = ("Calibri", "10")
         self.arquivo["width"] = 15
-        self.arquivo["command"] = self.widget1.after_idle
-        self.arquivo.grid()
+        self.arquivo ["command"] = self.widget1.grid
+        self.arquivo.pack()
+        self.painel = Button (self.widget1)
+        self.painel ["text"] = "Criar Grafico"
+        self.painel ["font"] = ("Calibri" , "10")
+        self.painel ["width"] = 15
+        self.painel ["command"] = self.widget1.anchor
+        self.painel.pack()
         self.sair = Button(self.widget1)
-        self.sair["text"] = "Sair"
+        self.sair["text"] = ("Sair")
         self.sair["font"] = ("Calibri", "10")
-        self.sair["width"] = 5
+        self.sair["width"] = 15
         self.sair["command"] = self.widget1.quit
-        self.sair.grid()
+        self.sair.pack()
+
 
 
 root = Tk()
